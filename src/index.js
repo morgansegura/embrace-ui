b54b4adf46a018e91fa145165e56a9eda45e35c7
Morgan Segura <morgansegura@gmail.com>
2019-12-31 11:32:49 -0800

    Adds snackbar to UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from './embrace-ui/tooltip'
 import Dropdown from './embrace-ui/dropdown'
 import Tabs from './embrace-ui/tabs'
+import Snackbar from './embrace-ui/snackbar'
 
 // Create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -17,5 +18,14 @@ dropdowns.forEach(dropdown => {
 // Create tabs
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
+
+// create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    snackbar.show('thanks for clicking me');
+});
 
 console.log("Embrace UI");
